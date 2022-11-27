3a386d7ae36def40539fa689c6b9be5e6a44b60d
unknown <al.mshary16@gmail.com>
2022-11-28 02:34:34 +0300

    side bar alternative

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,21 +1,15 @@
-// shift views in mobile between contacts and chat 
-const menus = document.querySelector('.side-bar');
-const chat = document.querySelector('.content-area');
-const examly_icon = document.querySelector('#examly-logo');
+// closing/expanding cards container
+const hideButtons =document.getElementsByClassName('hide-cards-btn')
 
-// chat.addEventListener('click', () => {
-//         chat.classList.toggle('view')
-//         menus.classList.toggle('view')
-// })
-/*
-change this effect to be for contacts (if he press any contact key he will be in the chat page)
-*/
-menus.addEventListener('click', () => {
-        chat.classList.toggle('view')
-        menus.classList.toggle('view')
+Array.from(hideButtons).forEach(element => {
+        element.addEventListener('click',(event)=>{
+        event.target.previousElementSibling.classList.toggle('hidden-cards-container')
+        if (!event.target.previousElementSibling.classList.contains('hidden-cards-container')) {
+                element.innerHTML= '- See less'
+        }
+        else {
+                element.innerHTML = '+ See more'
+        }
 })
-
-// function menuEffect(x) {
-//         x.classList.toggle("change");
-// }
+});
 
